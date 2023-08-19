bed4086aa59a11e4a8fb437e8d682ca1509f12c4
Riken Shah <rikenshah.02@gmail.com>
2023-08-19 14:03:38 +0530

    feat: use swin2SR

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import base64
+from io import BytesIO
 from potassium import Potassium, Request, Response
 
-from transformers import pipeline
+from transformers import AutoImageProcessor, Swin2SRForImageSuperResolution
+from PIL import Image
+import numpy as np
 import torch
 
 app = Potassium("my_app")
@@ -9,10 +13,12 @@ app = Potassium("my_app")
 @app.init
 def init():
     device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
-   
+    processor = AutoImageProcessor.from_pretrained("caidas/swin2SR-realworld-sr-x4-64-bsrgan-psnr")
+    model = Swin2SRForImageSuperResolution.from_pretrained("caidas/swin2SR-realworld-sr-x4-64-bsrgan-psnr")
+    
     context = {
-        "model": model
+        "model": model,
+        "processor": processor,
     }
 
     return context
@@ -20,12 +26,31 @@ def init():
 # @app.handler runs for every call
 @app.handler("/")
 def handler(context: dict, request: Request) -> Response:
-    prompt = request.json.get("prompt")
+    url = request.json.get("image")
+    processor = context.get("processor")
     model = context.get("model")
-    outputs = model(prompt)
+    # read base64 image
+    image = Image.open(BytesIO(base64.b64decode(url)))
+    # image = Image.open(requests.get(url, stream=True).raw)
+    # # prepare image for the model
+    inputs = processor(image, return_tensors="pt")
+    # # forward pass
+    with torch.no_grad():
+        outputs = model(**inputs)
+    output = outputs.reconstruction.data.squeeze().float().cpu().clamp_(0, 1).numpy()
+    output = np.moveaxis(output, source=0, destination=-1)
+    output = (output * 255.0).round().astype(np.uint8)
+
+    # # save output
+    output_image = Image.fromarray(output)
+    # png 
+    output_image_base64 = base64.b64encode(output_image.tobytes()).decode("utf-8")
+    data_url = f"data:image/png;base64,{output_image_base64}"
+    output_image.save("output.png")
+
 
     return Response(
-        json = {"outputs": outputs[0]}, 
+        json = {"outputs": data_url},
         status=200
     )
 
--- download.py
+++ download.py
@@ -2,10 +2,13 @@
 
 # In this example: A Huggingface BERT model
 from transformers import pipeline
+from transformers import AutoImageProcessor, Swin2SRForImageSuperResolution
+
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
+    Swin2SRForImageSuperResolution.from_pretrained("caidas/swin2SR-realworld-sr-x4-64-bsrgan-psnr")
+    # pipeline('fill-mask', model='bert-base-uncased')
 
 if __name__ == "__main__":
     download_model()
